bece79ba7e96a44a83006dff4f924bfd79ed97e9
CaiZhepeng <13580518380@163.com>
2019-08-29 19:37:58 +0800

    建立NGCamera项目

---
+++ NGCamera/NGCamera/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  NGCamera
+//
+//  Created by 蔡哲鹏 on 2019/8/29.
+//  Copyright © 2019 蔡哲鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
---
+++ NGCamera/NGCamera/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NGCamera
+//
+//  Created by 蔡哲鹏 on 2019/8/29.
+//  Copyright © 2019 蔡哲鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
